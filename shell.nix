507c22c874ca26c8caaba378d60400e835905b41
Martin Toman <whomenope@outlook.com>
2019-12-01 12:16:14 +0100

    Nix

---
+++ shell.nix
@@ -0,0 +1,24 @@
+let
+  # Look here for information about how to generate `nixpkgs-version.json`.
+  #  → https://nixos.wiki/wiki/FAQ/Pinning_Nixpkgs
+  pinnedVersion = builtins.fromJSON (builtins.readFile ./.nixpkgs-version.json);
+  pinnedPkgs = import (builtins.fetchGit {
+    inherit (pinnedVersion) url rev;
+
+    ref = "nixos-unstable";
+  }) {};
+  all-hies = import (fetchTarball "https://github.com/infinisil/all-hies/tarball/master") {};
+in
+
+# This allows overriding pkgs by passing `--arg pkgs ...`
+{ pkgs ? pinnedPkgs }:
+
+with pkgs;
+
+mkShell {
+  buildInputs = [
+    # put packages here.
+    stack
+    (all-hies.selection { selector = p: { inherit (p) ghc865; }; })
+  ];
+}
